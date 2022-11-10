e42fc205cd6467d3ca6a1ff460d4c1fc9c0ff0ec
tantaana <shariartanvirofficial@gmail.com>
2022-11-10 21:27:43 +0600

    abar

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -3,8 +3,8 @@ import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 const MyReviews = () => {
     const { user } = useContext(AuthContext);
-    console.log(user.email)
     const [reviews, setReviews] = useState([]);
+    console.log(reviews.length)
 
     useEffect(() => {
         fetch(`http://localhost:5000/reviewsUser?email=${user.email}`)
@@ -15,17 +15,8 @@ const MyReviews = () => {
         <div>
             <div className='px-10'>
                 <h3 className='text-2xl font-bold my-10'>Logged In User: <span className='text-red-400'>{user.email}</span></h3>
-                {
-                    reviews.map(review =>
-                        // <div className='flex p-4 gap-4 items-center'>
-                        //     <img src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png" className='w-1/6' alt="" />
-
-                        //     <div className=''>
-                        //         <h3 className='text-lg mb-6'>Service: <span className='font-bold text-yellow-500'>{review.title}</span></h3>
-                        //         <h3 className='text-md'>By <span className='font-bold text-red-400'>{review.user_Name}</span></h3>
-                        //         <h3 className='text-xl'>❝ {review.Message} ❞</h3>
-                        //     </div>
-                        // </div>
+                {reviews.length > 0 ?
+                    <>
                         <div className="overflow-x-auto w-full">
                             <table className="table w-full">
 
@@ -38,41 +29,47 @@ const MyReviews = () => {
                                         <th></th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                {
+                                    reviews.map(review =>
 
-                                    <tr>
-                                        <th>
-                                            <label>
-                                                <input type="checkbox" className="checkbox" />
-                                            </label>
-                                        </th>
-                                        <td>
-                                            <div className="flex items-center space-x-3">
-                                                <div className="avatar">
-                                                    <div className="mask mask-squircle w-12 h-12">
-                                                        <img src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png" alt="Avatar Tailwind CSS Component" />
+                                        <tbody>
+
+                                            <tr>
+                                                <th>
+                                                    <label>
+                                                        <input type="checkbox" className="checkbox" />
+                                                    </label>
+                                                </th>
+                                                <td>
+                                                    <div className="flex items-center space-x-3">
+                                                        <div className="avatar">
+                                                            <div className="mask mask-squircle w-12 h-12">
+                                                                <img src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png" alt="Avatar Tailwind CSS Component" />
+                                                            </div>
+                                                        </div>
+                                                        <div>
+                                                            <div className="font-bold text-xl">{review.user_Name}</div>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div>
-                                                    <div className="font-bold text-xl">{review.user_Name}</div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td className='font-bold text-yellow-500 text-xl'>
-                                            {review.title}
-                                        </td>
-                                        <td className='text-xl'>❝ {review.Message} ❞</td>
-                                        <th>
-                                            <button className="btn btn-ghost btn-xs">details</button>
-                                        </th>
-                                    </tr>
+                                                </td>
+                                                <td className='font-bold text-yellow-500 text-xl'>
+                                                    {review.title}
+                                                </td>
+                                                <td className='text-xl'>❝ {review.Message} ❞</td>
+                                                <th>
+                                                    <button className="btn btn-ghost btn-xs">details</button>
+                                                </th>
+                                            </tr>
 
-                                </tbody>
+                                        </tbody>
 
+
+                                    )
+                                }
                             </table>
                         </div>
-                    )
-                }
+                    </> :
+                    <h3 className='text-4xl font-bold text-red-500 text-center'>No User Review Found</h3>}
             </div>
         </div>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -21,7 +21,8 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li className='font-bold'><Link>Home</Link></li>
+                            <li className='font-bold'><Link to='/'>Home</Link></li>
+                            <li className='font-bold'><Link to='/services'>Services</Link></li>
                             <li className='font-bold'><Link to='/blogs'>Blogs</Link></li>
 
                             {user?.uid ?
@@ -50,6 +51,7 @@ const Header = () => {
                 <div className="navbar-center hidden xl:flex">
                     <ul className="menu menu-horizontal p-0">
                         <li><Link className='font-bold text-xl' to='/'>Home</Link></li>
+                        <li><Link className='font-bold text-xl' to='/services'>Services</Link></li>
                         <li><Link className='font-bold text-xl' to='/blogs'>Blogs</Link></li>
 
                         <li>{user?.uid ?
